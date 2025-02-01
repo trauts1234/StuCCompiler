f29eb74af9f0aad625a8f6b6fd6c817cdccc51fa
trauts1234 <stu-junk@outlook.com>
2025-02-01 19:49:26 +0000

    changed token indexes to slices

--- src/lexer/token_savepoint.rs
+++ src/lexer/token_savepoint.rs
@@ -1,22 +1,42 @@
-pub struct TokenQueueLocation {
-    index: usize
+/**
+ * specifies a slice of the token list
+ */
+pub struct TokenQueueSlice {
+    index: usize,
+    max_index: usize//where the end of the list is (so that you can slice)
 }
 
-impl TokenQueueLocation {
-    pub fn new() -> TokenQueueLocation{
-        TokenQueueLocation{
-            index:0
+impl TokenQueueSlice {
+    pub fn new() -> TokenQueueSlice{
+        TokenQueueSlice{
+            index:0,
+            max_index: usize::max_value()
         }
     }
-    pub fn from_previous_savestate(previous_location: &TokenQueueLocation) -> TokenQueueLocation {
-        TokenQueueLocation{
-            index: previous_location.index
+    pub fn from_previous_savestate(previous_location: &TokenQueueSlice) -> TokenQueueSlice {
+        TokenQueueSlice{
+            index: previous_location.index,
+            max_index: previous_location.max_index
         }
     }
 
     pub fn get_index(&self) -> usize{
         self.index
     }
+    /**
+     * get the index past the last item in the sliece
+     */
+    pub fn get_slice_max_idx(&self) -> usize {
+        self.max_index
+    }
+
+    /**
+     * returns the length of this slice
+     */
+    pub fn get_slice_size(&self) -> usize {
+        self.max_index - self.index
+    }
+
     pub fn next(&mut self){
         self.index += 1;
     }
--- src/lexer/token_walk.rs
+++ src/lexer/token_walk.rs
@@ -1,4 +1,4 @@
-use super::{token::Token, token_savepoint::TokenQueueLocation};
+use super::{token::Token, token_savepoint::TokenQueueSlice};
 
 /**
  * this steps through each token
@@ -17,22 +17,26 @@ impl TokenQueue {
     /**
      * returns the next token that needs to be consumed
      */
-    pub fn peek(&self, location: &TokenQueueLocation) -> Option<Token> {
+    pub fn peek(&self, location: &TokenQueueSlice) -> Option<Token> {
         let next_idx = location.get_index();
 
-        if next_idx >= self.tokens.len(){
+        if next_idx >= self.tokens.len() || next_idx >= location.get_slice_max_idx(){
             return None;//run out of tokens
         }
 
         Some(self.tokens[next_idx].clone())
     }
 
-    pub fn consume(&mut self, location: &mut TokenQueueLocation) -> Option<Token> {
+    pub fn consume(&mut self, location: &mut TokenQueueSlice) -> Option<Token> {
         let next = self.peek(&location);
         location.next();
-        if location.get_index() > self.tokens.len(){
-            panic!("continued consuming tokens after end of array")
+        if location.get_index() > self.tokens.len() || location.get_index() > location.get_slice_max_idx(){
+            panic!("continued consuming tokens after end of array or past end of allowed slice")
         }
         return next;
+    }
+
+    pub fn get_slice(&self, slice: &TokenQueueSlice) -> &[Token] {
+        &self.tokens[slice.get_index()..slice.get_slice_max_idx()]
     }
 }
