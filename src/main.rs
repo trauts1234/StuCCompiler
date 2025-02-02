2ac1b88858dc8fc994729daa11f04b60640a379c
trauts1234 <stu-junk@outlook.com>
2025-02-02 21:13:09 +0000

    added linker and assembler call to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 use translation_unit::TranslationUnit;
 
 mod function_definition;
@@ -20,4 +22,21 @@ fn main() {
     println!("{:#?}", tu);
 
     tu.generate_assembly("a.asm");
+
+    //assemble
+    let nasm_status = Command::new("nasm")
+        .arg("-f elf64")
+        .arg("a.asm")
+        .status()
+        .expect("Failed to run NASM");
+    assert!(nasm_status.success(), "NASM failed to assemble the file");
+
+    //link
+    let ld_status = Command::new("ld")
+        .arg("a.o")
+        .arg("-o")
+        .arg("a.out")
+        .status()
+        .expect("Failed to run linker");
+    assert!(ld_status.success(), "Linker failed to produce the binary");
 }
