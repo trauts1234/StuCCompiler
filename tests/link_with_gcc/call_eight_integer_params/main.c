ad36c1a793e2363dba1651922030d11b9e0fbe55
trauts1234 <stu-junk@outlook.com>
2025-03-29 22:25:23 +0000

    added eight param test

--- tests/link_with_gcc/call_eight_integer_params/main.c
+++ tests/link_with_gcc/call_eight_integer_params/main.c
@@ -1,5 +1,5 @@
-extern int func(int p1, int p2, int p3, int p4, int p5, int p6);
+extern int func(int p1, int p2, int p3, char p4, int p5, int p6, char p7, int p8);
 
 int main() {
-    return func(1,2,3,4,5,6);
+    return func(1,2,3,4,5,6,7,8);
 }
