54acb10ecf5cc6da23d0e54f1909829999ddd586
trauts1234 <stu-junk@outlook.com>
2025-07-15 22:38:54 +0100

    added reverse test, found a bug

---
+++ tests/link_with_gcc/called_structs/gcc_code.c
@@ -0,0 +1,56 @@
+struct x {int a;};//passed by 1 reg
+
+extern int f1(int p1, struct x p2);//test simple struct pass by value
+
+extern int f2(int p1, struct x *p2_ptr);//test pass by pointer
+
+extern int f3(int p1, struct x p2, int p3, int p4, int p5, int p6, int p7);//struct gets passed by register
+
+struct y {int a;int b;char c;};//passed by 2 regs
+
+extern int f4(int p1, struct y p2, int p3, int p4, int p5, int p6, int p7); // uses 2 registers
+
+extern int f5(int p1, int p2, int p3, int p4, int p5, struct y p6, int p7); // cannot fit in 1 reg, so goes on stack, and p7 goes in reg
+
+struct z {long int a;int b;long int c;};//does not fit in registers
+
+extern int f6(struct z p1, int p2);
+
+extern int f7(struct y p1);
+
+int main() {
+    struct x foo;
+    foo.a = 1;
+    struct y bar;
+    bar.a = 1;
+    bar.b = 2;
+    bar.c = 3;
+    struct z baz;
+    baz.a = 1;
+    baz.b = 2;
+    baz.c = 3;
+
+    if(f1(1,foo)) {
+        return 1;
+    }
+    if(f2(1, &foo)) {
+        return 2;
+    }
+    if(f3(1, foo, 3, 4, 5, 6, 7)) {
+        return 3;
+    }
+    if(f4(1,bar, 3, 4, 5, 6, 7)) {
+        return 4;
+    }
+    
+    if(f5(1,2,3,4,5,bar,7)) {
+        return 5;
+    }
+    if(f6(baz, 2)) {
+        return 6;
+    }
+    if(f7(bar)) {
+        return 7;
+    }
+    return 0;
+}
---
+++ tests/link_with_gcc/called_structs/main.c
@@ -0,0 +1,31 @@
+struct x { int a; };
+struct y { int a; int b; char c; };
+struct z {long int a;int b;long int c;};
+
+int f1(int p1, struct x p2) {
+    return !(p1 == 1 && p2.a == 1);
+}
+
+int f2(int p1, struct x *p2_ptr) {
+    return !(p1 == 1 && p2_ptr && (*p2_ptr).a == 1);
+}
+
+int f3(int p1, struct x p2, int p3, int p4, int p5, int p6, int p7) {
+    return !(p1 == 1 && p2.a == 1 && p3 == 3 && p4 == 4 && p5 == 5 && p6 == 6 && p7 == 7);
+}
+
+int f4(int p1, struct y p2, int p3, int p4, int p5, int p6, int p7) {
+    return !(p1 == 1 && p2.a == 1 && p2.b == 2 && p2.c == 3 && p3 == 3 && p4 == 4 && p5 == 5 && p6 == 6 && p7 == 7);
+}
+
+int f5(int p1, int p2, int p3, int p4, int p5, struct y p6, int p7) {
+    return !(p1 == 1 && p2 == 2 && p3 == 3 && p4 == 4 && p5 == 5 && p6.a == 1 && p6.b == 2 && p6.c == 3 && p7 == 7);
+}
+
+int f6(struct z p1, int p2){
+    return !(p1.a == 1 && p1.b == 2 && p1.c == 3 && p2 == 2);
+}
+
+int f7(struct y p1) {
+    return !(p1.a == 1 && p1.b == 2 && p1.c == 3);
+}
